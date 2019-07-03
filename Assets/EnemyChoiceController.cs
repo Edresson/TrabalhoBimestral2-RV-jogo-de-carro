9b6a92e5c05bc0da3e145d102823546abd01d82a
Felipe Divensi <divensi@Felipes-iMac.local>
2019-07-03 18:54:41 -0300

    Escolha de dificuldade, minimapa com inimigos

---
+++ Assets/EnemyChoiceController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChoiceController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.childCount > 0)
+        {
+            string dificuldade = PlayerPrefs.GetString("Dificuldade", "Facil"); 
+
+            // desativa todos
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                GameObject Go = transform.GetChild(i).gameObject;
+                if (Go != null) {
+                    // se for igual ao nome da dificuldade ativa, senao desativa
+                    Go.SetActive(Go.name.Equals(dificuldade));
+                }
+            }
+        }
+    }
+}
